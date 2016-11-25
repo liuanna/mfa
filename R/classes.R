7524a2db07f2f9659322a2766859fee1e89b9520
Anna Liu <liuannaceo@gmail.com>
2016-11-24 16:15:25 -0800

    add mfa class and main function mfa

--- R/classes.R
+++ R/classes.R
@@ -1,15 +1,19 @@
 # create a S4 class for mfa
-
 setClass(
   Class = "mfa",
   representation = representation(
     ncomps = "numeric",
     center = "logical",
-    scale = "logical"
+    scale = "logical",
+    eigenvalues = "numeric",
+    cfs = "matrix",
+    pfs = "list",
+    loadings = "matrix"
   ),
   prototype = prototype(
-    ncomps = NULL,
     center = TRUE,
     scale = TRUE
   )
 )
+
+
